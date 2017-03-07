7b270d4764dd0f4562bf3019ae6cb4dde1acd334
Tina Lam <tina.lam516@gmail.com>
2017-03-07 11:00:44 -0500

    streamlined

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require( 'express' );
 const app = express();
+const chalk = require( 'chalk' );
+const volleyball = require( 'volleyball' );
 
 app.listen(3000, function() {
-  console.log("Hi, I'm listening!");
+  console.log(chalk.bgYellow("Hi, I'm listening!"));
+})
+
+// app.use(function (request, response, next) {
+//     console.log(chalk.cyan(request.method + " " + request.originalUrl + " " + response.statusCode));
+//     next();
+// })
+app.use(volleyball);
+
+app.use('/special', function(request, response) {
+  response.send("You've reached the special area.")
 })
 
 app.get('/home', function(request, response) {
-  response.send("This is the home page.");   
+  response.send("This is the home page.");
+})
+
+app.get('/news', function(request, response) {
+  response.send("Here's the news, you're lost! :[");
 })
